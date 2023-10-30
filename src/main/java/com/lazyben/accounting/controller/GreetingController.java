77f554baa9d40ee586dcfe4c6f16f89a43d60874
lazyben <458861463@qq.com>
2023-10-30 10:26:21 +0800

    feat(accounting): test springboot using a simple greeting controller.

---
+++ src/main/java/com/lazyben/accounting/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.lazyben.accounting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+    @GetMapping("/hello")
+    public String greeting() {
+        return "Hello World!";
+    }
+}
