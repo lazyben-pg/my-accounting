71b0052ffbe60b65b3870e063a7d11d3ab7c80ba
lazyben <458861463@qq.com>
2023-12-27 14:48:47 +0800

    chore(accounting): use lombok replace setter and getter.

--- src/main/java/com/lazyben/accounting/model/dataobject/UserInfoDO.java
+++ src/main/java/com/lazyben/accounting/model/dataobject/UserInfoDO.java
@@ -1,7 +1,13 @@
 package com.lazyben.accounting.model.dataobject;
 
+import lombok.*;
+
 import java.util.Date;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserInfoDO {
     private Long id;
 
@@ -12,44 +18,4 @@ public class UserInfoDO {
     private Date createTime;
 
     private Date updateTime;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
